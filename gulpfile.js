f2691b8466f91a7a9247eb59d79648413f9218ce
William Ewerton <william_ewerton@outlook.com>
2017-07-06 16:49:44 -0300

    gt

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
 
 gulp.task('default', function() {
-  // place code for your default task here
+  //gulp.watch('sass/**/*.scss',['styles']);
+
+  gulp.src('./config.exemple.php')
+        .pipe(gulp.dest('./', ['config.php']));
 });
